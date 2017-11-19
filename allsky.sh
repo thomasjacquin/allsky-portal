cb9345016e1f85619761d0d4b4f64083277df16f
Thomas Jacquin <jacquin.thomas@gmail.com>
2017-11-19 00:10:56 -0800

    ADDED timelapse and save image to repo to overwrite originals

--- allsky.sh
+++ allsky.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 echo "Starting allsky camera..."
-#cd /home/pi/allsky
+cd /home/pi/allsky
 
 # Set upload to false if you don't want to upload the latest image to your website
 UPLOAD=false
-
-# Set -help to 1 for full list of arguments
-# Set -nodisplay to 1 on Raspbian Lite and other OS without Desktop environments
-
 CAMERA_SETTINGS='/var/www/html/settings.json'
-
 ARGUMENTS=""
 
 KEYS=( $(jq -r 'keys[]' $CAMERA_SETTINGS) )
---
+++ saveImage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CAMERA_SETTINGS='/var/www/html/settings.json'
+FILENAME=$(jq -r '.filename' $CAMERA_SETTINGS)
+EXTENSION="${FILENAME##*.}"
+FILENAME="${FILENAME%.*}"
+UPLOAD=$2
+
+# Make a directory to store current night images
+mkdir -p images/current;
+cp "$1" "images/current/$FILENAME-$(date +'%Y%m%d%H%M%S').$EXTENSION";
+
+if [ "$UPLOAD" = true ] ; then
+    	echo "Resizing\n";
+	
+	# Create a thumbnail for live view
+	# Here's what I use with my ASI224MC
+	convert "$1" -resize 962x720 -gravity East -chop 2x0 "$FILENAME-resize.$EXTENSION";
+	# Here's what I use with my ASI185MC (larger sensor so I crop the black around the image)
+	#convert "$1" -resize 962x720 -gravity Center -crop 680x720+40+0 +repage "$FILENAME-resize.$EXTENSION";
+	
+	#echo "Uploading\n";
+	lftp sftp://user:password@host:/path/to/website -e "put $FILENAME-resize.$EXTENSION; bye"
+
+fi
+
