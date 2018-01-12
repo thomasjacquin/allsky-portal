a08ac19d503e365b8c1bc96a568f7c5ba30374c0
Thomas Jacquin <jacquin.thomas@gmail.com>
2018-01-12 11:10:22 -0800

    Added script to save camera settings without rebooting

---
+++ restartCapture.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /home/pi/allsky/config.sh
+
+echo "Restarting Capture with new settings"
+cd /home/pi/allsky
+
+# Building the arguments to pass to the capture binary
+ARGUMENTS=""
+KEYS=( $(jq -r 'keys[]' $CAMERA_SETTINGS) )
+for KEY in ${KEYS[@]}
+do
+	ARGUMENTS="$ARGUMENTS -$KEY `jq -r '.'$KEY $CAMERA_SETTINGS` "
+done
+echo "$ARGUMENTS">>log.txt
+
+# We kill the capture process and restart it with new arguments
+killall -9 capture ; ./capture $ARGUMENTS
