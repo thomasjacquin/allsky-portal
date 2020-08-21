fb914971837e805dba9a21012c58922eb7148f23
Thomas Jacquin <jacquin.thomas@gmail.com>
2020-08-20 20:59:41 -0700

    Fixed public.php page

--- public.php
+++ public.php
@@ -1,7 +1,27 @@
 <?php
 
   define('RASPI_CONFIG', '/etc/raspap');
-  define('RASPI_CAMERA_SETTINGS', RASPI_CONFIG.'/settings.json');
+
+  $file = '/home/pi/allsky/config.sh';
+  $searchfor = 'CAMERA=';
+
+  // get the file contents, assuming the file to be readable (and exist)
+  $contents = file_get_contents($file);
+  // escape special characters in the query
+  $pattern = preg_quote($searchfor, '/');
+  // finalise the regular expression, matching the whole line
+  $pattern = "/^.*$pattern.*\$/m";
+  // search, and store all matching occurences in $matches
+  if(preg_match_all($pattern, $contents, $matches)){
+        $double_quote = '"';
+        $cam = str_replace($double_quote, '', explode( '=', implode("\n", $matches[0]))[1]);
+  }
+  else{
+    $cam = "ZWO";
+  }
+
+  define('RASPI_CAMERA_SETTINGS', RASPI_CONFIG . '/settings_'.$cam.'.json');
+
   $camera_settings_str = file_get_contents(RASPI_CAMERA_SETTINGS, true);
   $camera_settings_array = json_decode($camera_settings_str, true);
 
