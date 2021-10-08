db3ee9634ce6c3762fe891812dea227f978fccfe
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-07 20:02:52 -0500

    added ALLSKY_SCRIPTS

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
 
 include_once('includes/functions.php');		// needs to be at top for get_variable()
 
-define('ALLSKY_HOME', '/home/pi/allsky');		// value updated during installation
+define('ALLSKY_HOME', '/home/pi/allsky');			// value updated during installation
+define('ALLSKY_SCRIPTS', ALLSKY_HOME . '/scripts');	// value updated during installation
 
 // xxx COMPATIBILITY CHECK:
 // Version 0.8 and older of allsky had config.sh and autocam.sh in $ALLSKY_HOME.
