046b5fcbe5a678500638e935dbe46034fff228dc
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-13 23:42:50 -0500

    save_file.php: error checking
    Check if unable to save file.

--- includes/save_file.php
+++ includes/save_file.php
@@ -5,16 +5,27 @@ $content = "";
 $path = "";
 
 if (isset($_POST['content'])) {
-    $content = $_POST['content'];
+	$content = $_POST['content'];
 }
 
 if (isset($_POST['path'])) {
-    $path = $_POST['path'];
+	$path = $_POST['path'];
+}
+if ($path == "") {
+	echo "save_file.php: Unknown path to save to, is null";
+	exit;
 }
 
 $file = str_replace('current', ALLSKY_HOME, $path);
-
-file_put_contents(getcwd()."/temp", $content);
-shell_exec("sudo mv ".getcwd()."/temp ".$file . ";sudo chown pi:pi ".$file . ";sudo chmod +x ".$file);
-
+$tempFile = getcwd() . "/temp";
+if (file_put_contents($tempFile, $content) == false) {
+    echo "<p style='color: red'>Unable to save to temporary file '$tempFile'.</p>";
+    echo "<br>Contents:<br><pre>$content</pre>";
+	exit;
+} else {
+	// This shouldn't return anything unless there is an error.
+	$msg = shell_exec("sudo mv '$tempFile' '$file' || echo 'Unable to mv $tempFile to $file'; sudo chown pi:pi '$file'; sudo chmod +x '$file'");
+	if ($msg != "")
+		echo "<p style='color: red'>save_file.php: $msg</p>";
+}
 ?>
