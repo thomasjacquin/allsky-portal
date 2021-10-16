429fc2fa63282749d21be1531be2af9ae33c9b56
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-16 02:36:54 -0500

    save_file.php: display error messages

--- includes/save_file.php
+++ includes/save_file.php
@@ -4,6 +4,8 @@ define('ALLSKY_HOME', '/home/pi/allsky');                     // value updated d
 $content = "";
 $path = "";
 
+// On error, return a string.  On success, return nothing.
+
 if (isset($_POST['content'])) {
 	$content = $_POST['content'];
 }
@@ -12,20 +14,25 @@ if (isset($_POST['path'])) {
 	$path = $_POST['path'];
 }
 if ($path == "") {
-	echo "save_file.php: Unknown path to save to, is null";
+	echo "save_file.php: Path to save to is null";
 	exit;
 }
 
+// "current" is a web alias to ALLSKY_HOME.
 $file = str_replace('current', ALLSKY_HOME, $path);
 $tempFile = getcwd() . "/temp";
 if (file_put_contents($tempFile, $content) == false) {
-    echo "<p style='color: red'>Unable to save to temporary file '$tempFile'.</p>";
-    echo "<br>Contents:<br><pre>$content</pre>";
+	echo error_get_last()['message'];
 	exit;
 } else {
-	// This shouldn't return anything unless there is an error.
-	$msg = shell_exec("sudo mv '$tempFile' '$file' || echo 'Unable to mv $tempFile to $file'; sudo chown pi:pi '$file'; sudo chmod +x '$file'");
-	if ($msg != "")
-		echo "<p style='color: red'>save_file.php: $msg</p>";
+	// shell_exec() doesn't return anything unless there is an error.
+	$msg = shell_exec("sudo mv '$tempFile' '$file' || echo 'Unable to mv [$tempFile] to [$file]'");
+	if ($msg == "") {
+		shell_exec("sudo chown pi:pi '$file'; sudo chmod +x '$file'");
+	} else {
+		//header("HTTP/1.0 400 Bad Request");
+		echo "save_file.php: $msg";
+		exit;
+	}
 }
 ?>
