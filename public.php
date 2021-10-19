8e0c9a08e3df5aa57d4219f65f89441d2598eefb
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-10-19 02:05:15 -0500

    public.php: centralize code
    Move many of the constant and variable setting code to functions.php since it was shared with index.php and more difficult to maintain.

--- public.php
+++ public.php
@@ -1,76 +1,53 @@
 <?php
 
+// functions.php sets a bunch of constants and variables,
+// including $image_name and $delay.
+// It needs to be at the top of this file since code below uses the items it sets.
 include_once('includes/functions.php');
-
-define('ALLSKY_HOME', '/home/pi/allsky');		// value updated during installation
-define('ALLSKY_SCRIPTS', ALLSKY_HOME . '/scripts');	// value updated during installation
-define('ALLSKY_IMAGES', ALLSKY_HOME . '/images');	// value updated during installation
-
-// xxx COMPATIBILITY CHECK:
-// Version 0.8 and older of allsky had config.sh and autocam.sh in $ALLSKY_HOME.
-// Newer versions have them in $ALLSKY_CONFIG.
-// Look for a variable we know won't be null in the new location;
-// if it's not there, use the old location.
-$allsky_config_dir = '/config';
-$allsky_config = ALLSKY_HOME . $allsky_config_dir;	// value updated during installation
-if (file_exists($allsky_config . '/config.sh')) {
-	define('ALLSKY_CONFIG_DIR', '/config');
-	define('ALLSKY_CONFIG', $allsky_config);
-} else {
-	define('ALLSKY_CONFIG_DIR', '');
-	define('ALLSKY_CONFIG', ALLSKY_HOME);
-}
-
-$img_dir = get_variable(ALLSKY_CONFIG . '/config.sh', 'IMG_DIR=', 'current');
-$cam = get_variable(ALLSKY_CONFIG . '/autocam.sh', 'CAMERA=', 'ZWO');
-define('RASPI_CONFIG', '/etc/raspap');			// xxx will replace with ALLSKY_CONFIG
-define('RASPI_CAMERA_SETTINGS', RASPI_CONFIG . '/settings_'.$cam.'.json');
-
-$camera_settings_str = file_get_contents(RASPI_CAMERA_SETTINGS, true);
-$camera_settings_array = json_decode($camera_settings_str, true);
-// xxx NEW:  $image_name = $img_dir . "/" . $camera_settings_array['filename'];
-// // old way uses IMG_PREFIX:
-$img_prefix = get_variable(ALLSKY_CONFIG . '/config.sh', 'IMG_PREFIX=', '');
-$image_name = $img_dir . "/" . $img_prefix . $camera_settings_array['filename'];
-
 ?>
 
-<style>
-body {
-	margin: 0;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style> body { margin: 0; } </style>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Thomas Jacquin">
+	<title>AllSky Public Page</title>
+</head>
+<body>
 
 <div class="row">
-   <div id="live_container" style="background-color: black;">
-   	<img id="current" class="current" src="<?php echo $image_name ?>" style="width:100%">
-   </div>
+	<div id="live_container" style="background-color: black;">
+		<img id="current" class="current" src="<?php echo $image_name ?>" style="width:100%">
+	</div>
 </div>
 
-<!-- jQuery -->
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
 
 <script type="text/javascript">
-                function getImage(){
-                        var img = $("<img />").attr('src', '<?php echo $image_name ?>?_ts=' + new Date().getTime())
+	function getImage(){
+		var img = $("<img />").attr('src', '<?php echo $image_name ?>?_ts=' + new Date().getTime())
+			.attr("id", "current")
+			.attr("class", "current")
+			.css("width", "100%")
+			.on('load', function() {
+				if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+					console.log('broken image!');
+					setTimeout(function(){
+						getImage();
+					}, 500);
+				} else {
+					$("#live_container").empty().append(img);
+				}
+			});
+	}
 
-                                .attr("id", "current")
-                                .attr("class", "current")
-                                .css("width", "100%")
-                                .on('load', function() {
-                                    if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-                                        console.log('broken image!');
-                                        setTimeout(function(){
-                                            getImage();
-                                        }, 500);
-                                    } else {
-                                        $("#live_container").empty().append(img);
-                                    }
-                                });
-                }
-
-                setInterval(function(){
-                        getImage();
-                }, <?php echo $camera_settings_array["nightexposure"]/1000 < 5000 ? 5000 : $camera_settings_array["nightexposure"]/1000?>);
-
- </script>
+	setInterval(function(){
+		getImage();
+	}, <?php echo $delay ?>);
+</script>
+</body>
+</html>
