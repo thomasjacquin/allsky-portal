5b4c0a4ee6ac24376c96afffc8252519225f6c21
EricClaeys <83164203+EricClaeys@users.noreply.github.com>
2021-12-23 22:23:18 -0600

    system.php: convert spaces to tabs
    Also removed an unneeded "elseif".

--- includes/system.php
+++ includes/system.php
@@ -8,9 +8,9 @@
 
 function RPiVersion()
 {
-    // Lookup table from https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md
-    // Last updated december 2020
-    $revisions = array(
+	// Lookup table from https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md
+	// Last updated december 2020
+	$revisions = array(
 	'0002' => 'Model B Revision 1.0',
 	'0003' => 'Model B Revision 1.0 + ECN0001',
 	'0004' => 'Model B Revision 2.0 (256 MB)',
@@ -57,22 +57,22 @@ function RPiVersion()
 	'c03114' => 'Model 4B Revision 1.4 (4 GB)',
 	'd03114' => 'Model 4B Revision 1.4 (8 GB)',
 	'c03130' => 'Pi 400 Revision 1.0 (4 GB)'
-    );
-    // space and tab for -F
-    exec("awk -F ' ' '/^Revision/ {print $3; exit 0;}' < /proc/cpuinfo", $rev);
-    $rev = trim(array_pop($rev));
-    if (array_key_exists($rev, $revisions)) {
-        return $revisions[$rev];
-    } else {
-        return 'Unknown Pi, rev=' . $rev;
-    }
+	);
+	// space and tab for -F
+	exec("awk -F ' '   '/^Revision/ {print $3; exit 0;}' < /proc/cpuinfo", $rev);
+	$rev = trim(array_pop($rev));
+	if (array_key_exists($rev, $revisions)) {
+		return $revisions[$rev];
+	} else {
+		return 'Unknown Pi, rev=' . $rev;
+	}
 }
 
 function formatSize($bytes)
 {
-    $types = array('B', 'KB', 'MB', 'GB', 'TB');
-    for ($i = 0; $bytes >= 1024 && $i < (count($types) - 1); $bytes /= 1024, $i++) ;
-    return (round($bytes, 2) . " " . $types[$i]);
+	$types = array('B', 'KB', 'MB', 'GB', 'TB');
+	for ($i = 0; $bytes >= 1024 && $i < (count($types) - 1); $bytes /= 1024, $i++) ;
+	return (round($bytes, 2) . " " . $types[$i]);
 }
 
 /**
@@ -81,9 +81,9 @@ function formatSize($bytes)
  */
 function DisplaySystem()
 {
-    $top_dir = "/var/www";
-    $camera_settings_str = file_get_contents(RASPI_CAMERA_SETTINGS, true);
-    $camera_settings_array = json_decode($camera_settings_str, true);
+	$top_dir = "/var/www";
+	$camera_settings_str = file_get_contents(RASPI_CAMERA_SETTINGS, true);
+	$camera_settings_array = json_decode($camera_settings_str, true);
 	if (isset($camera_settings_array['temptype'])) {
 		$temp_type = $camera_settings_array['temptype'];
 		if ($temp_type == "") $temp_type = "C";
@@ -91,62 +91,62 @@ function DisplaySystem()
 		$temp_type = "C";
 	}
 
-    // hostname
-    exec("hostname -f", $hostarray);
-    $hostname = $hostarray[0];
+	// hostname
+	exec("hostname -f", $hostarray);
+	$hostname = $hostarray[0];
 
-    // uptime
-    $uparray = explode(" ", exec("cat /proc/uptime"));
-    $seconds = round($uparray[0], 0);
-    $minutes = $seconds / 60;
-    $hours = $minutes / 60;
-    $days = floor($hours / 24);
-    $hours = floor($hours - ($days * 24));
-    $minutes = floor($minutes - ($days * 24 * 60) - ($hours * 60));
-    $uptime = '';
-    if ($days != 0) {
-        $uptime .= $days . ' day' . (($days > 1) ? 's ' : ' ');
-    }
-    if ($hours != 0) {
-        $uptime .= $hours . ' hour' . (($hours > 1) ? 's ' : ' ');
-    }
-    if ($minutes != 0) {
-        $uptime .= $minutes . ' minute' . (($minutes > 1) ? 's ' : ' ');
-    }
+	// uptime
+	$uparray = explode(" ", exec("cat /proc/uptime"));
+	$seconds = round($uparray[0], 0);
+	$minutes = $seconds / 60;
+	$hours = $minutes / 60;
+	$days = floor($hours / 24);
+	$hours = floor($hours - ($days * 24));
+	$minutes = floor($minutes - ($days * 24 * 60) - ($hours * 60));
+	$uptime = '';
+	if ($days != 0) {
+		$uptime .= $days . ' day' . (($days > 1) ? 's ' : ' ');
+	}
+	if ($hours != 0) {
+		$uptime .= $hours . ' hour' . (($hours > 1) ? 's ' : ' ');
+	}
+	if ($minutes != 0) {
+		$uptime .= $minutes . ' minute' . (($minutes > 1) ? 's ' : ' ');
+	}
 
-    // mem used
-    exec("free -m | awk '/Mem:/ { total=$2 } /buffers\/cache/ { used=$3 } END { print used/total*100}'", $memarray);
-    $memused = floor($memarray[0]);
-    // check for memused being unreasonably low, if so repeat expecting modern output of "free" command
-    if ($memused < 0.1) {
-        unset($memarray);
-        exec("free -m | awk '/Mem:/ { total=$2 } /Mem:/ { used=$3 } END { print used/total*100}'", $memarray);
-        $memused = floor($memarray[0]);
-    }
-    if ($memused > 90) {
-        $memused_status = "danger";
-    } elseif ($memused > 75) {
-        $memused_status = "warning";
-    } elseif ($memused > 0) {
-        $memused_status = "success";
-    }
+	// mem used
+	exec("free -m | awk '/Mem:/ { total=$2 } /buffers\/cache/ { used=$3 } END { print used/total*100}'", $memarray);
+	$memused = floor($memarray[0]);
+	// check for memused being unreasonably low, if so repeat expecting modern output of "free" command
+	if ($memused < 0.1) {
+		unset($memarray);
+		exec("free -m | awk '/Mem:/ { total=$2 } /Mem:/ { used=$3 } END { print used/total*100}'", $memarray);
+		$memused = floor($memarray[0]);
+	}
+	if ($memused > 90) {
+		$memused_status = "danger";
+	} elseif ($memused > 75) {
+		$memused_status = "warning";
+	} elseif ($memused > 0) {
+		$memused_status = "success";
+	}
 
 
-    // Disk usage
-    // File Usage
-    /* get disk space free (in bytes) */
-    $df = disk_free_space($top_dir);
-    /* and get disk space total (in bytes)  */
-    $dt = disk_total_space($top_dir);
-    /* now we calculate the disk space used (in bytes) */
-    $du = $dt - $df;
-    /* percentage of disk used - this will be used to also set the width % of the progress bar */
-    $dp = sprintf('%.1f', ($du / $dt) * 100);
+	// Disk usage
+	// File Usage
+	/* get disk space free (in bytes) */
+	$df = disk_free_space($top_dir);
+	/* and get disk space total (in bytes)  */
+	$dt = disk_total_space($top_dir);
+	/* now we calculate the disk space used (in bytes) */
+	$du = $dt - $df;
+	/* percentage of disk used - this will be used to also set the width % of the progress bar */
+	$dp = sprintf('%.1f', ($du / $dt) * 100);
 
-    /* and we format the size from bytes to MB, GB, etc. */
-    $df = formatSize($df);
-    $du = formatSize($du);
-    $dt = formatSize($dt);
+	/* and we format the size from bytes to MB, GB, etc. */
+	$df = formatSize($df);
+	$du = formatSize($du);
+	$dt = formatSize($dt);
 
 	// Throttle / undervoltage status
 	$x = exec("sudo vcgencmd get_throttled 2>&1");	// Output: throttled=0x12345...
@@ -183,99 +183,99 @@ function DisplaySystem()
 				$bit = $bits[$i];
 				if ($bit == 0) continue;
 				if (array_key_exists($pos, $messages)) {
-                	if ($throttle == "") {
-                    	$throttle = $messages[$pos];
+					if ($throttle == "") {
+						$throttle = $messages[$pos];
 					} else {
-				    	$throttle .= "<br>" . $messages[$pos];
-                	}
+						$throttle .= "<br>" . $messages[$pos];
+					}
 					// current issues are a danger; prior issues are a warning
-					if ($pos <=3) $throttle_status = "danger";
+					if ($pos <= 3) $throttle_status = "danger";
 				}
 			}
 		}
 	}
 
-    // cpu load
-    $secs = 2; $q = '"';
-    $cpuload = exec("(grep -m 1 'cpu ' /proc/stat; sleep $secs; grep -m 1 'cpu ' /proc/stat) | awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else printf($q%.0f$q, (($2+$4-u1) * 100 / (t-t1))); }'");
-    if ($cpuload < 0 || $cpuload > 100) echo "<p style='color: red; font-size: 125%;'>Invalid cpuload value: $cpuload</p>";
-    if ($cpuload > 90 || $cpuload < 0) {
-        $cpuload_status = "danger";
-    } elseif ($cpuload > 75) {
-        $cpuload_status = "warning";
-    } elseif ($cpuload >= 0) {
-        $cpuload_status = "success";
-    }
+	// cpu load
+	$secs = 2; $q = '"';
+	$cpuload = exec("(grep -m 1 'cpu ' /proc/stat; sleep $secs; grep -m 1 'cpu ' /proc/stat) | awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else printf($q%.0f$q, (($2+$4-u1) * 100 / (t-t1))); }'");
+	if ($cpuload < 0 || $cpuload > 100) echo "<p style='color: red; font-size: 125%;'>Invalid cpuload value: $cpuload</p>";
+	if ($cpuload > 90 || $cpuload < 0) {
+		$cpuload_status = "danger";
+	} elseif ($cpuload > 75) {
+		$cpuload_status = "warning";
+	} else {
+		$cpuload_status = "success";
+	}
 
-    // temperature
-    $temperature = round(exec("awk '{print $1/1000}' /sys/class/thermal/thermal_zone0/temp"), 2);
-    if ($temperature > 70 || $temperature < 0) {
-        $temperature_status = "danger";
-    } elseif ($temperature > 60 || $temperature < 10) {
-        $temperature_status = "warning";
-    } else {
-        $temperature_status = "success";
-    }
-    $display_temperature = "";
-    if ($temp_type == "C" || $temp_type == "B")
-        $display_temperature = number_format($temperature, 1, '.', '') . "&deg;C";
-    if ($temp_type == "F" || $temp_type == "B")
-        $display_temperature = $display_temperature . "&nbsp; &nbsp;" . number_format((($temperature * 1.8) + 32), 1, '.', '') . "&deg;F";
+	// temperature
+	$temperature = round(exec("awk '{print $1/1000}' /sys/class/thermal/thermal_zone0/temp"), 2);
+	if ($temperature > 70 || $temperature < 0) {
+		$temperature_status = "danger";
+	} elseif ($temperature > 60 || $temperature < 10) {
+		$temperature_status = "warning";
+	} else {
+		$temperature_status = "success";
+	}
+	$display_temperature = "";
+	if ($temp_type == "C" || $temp_type == "B")
+		$display_temperature = number_format($temperature, 1, '.', '') . "&deg;C";
+	if ($temp_type == "F" || $temp_type == "B")
+		$display_temperature = $display_temperature . "&nbsp; &nbsp;" . number_format((($temperature * 1.8) + 32), 1, '.', '') . "&deg;F";
 
-    // fan speed.  Should probably put the path in config.sh...
-    $fan_data = "/home/pi/fan/fandata.txt";
-    if (file_exists($fan_data)) {	// fanspeed is $1, we want percent which is $2
-        $fan = exec("awk '{print $2}' ".$fan_data);
-        if ($fan >= 90) {
-	        $fan_status = "danger";
-        } elseif ($fan >= 75) {
-	        $fan_status = "warning";
-        } else {
-	        $fan_status = "success";
-        }
-    } else {
-        $fan = "";
-    }
+	// fan speed.  Should probably put the path in config.sh...
+	$fan_data = "/home/pi/fan/fandata.txt";
+	if (file_exists($fan_data)) {	// fanspeed is $1, we want percent which is $2
+		$fan = exec("awk '{print $2}' ".$fan_data);
+		if ($fan >= 90) {
+			$fan_status = "danger";
+		} elseif ($fan >= 75) {
+			$fan_status = "warning";
+		} else {
+			$fan_status = "success";
+		}
+	} else {
+		$fan = "";
+	}
 
-    // disk usage
-    if ($dp > 90) {
-        $disk_usage_status = "danger";
-    } elseif ($dp > 70 && $dp < 90) {
-        $disk_usage_status = "warning";
-    } else {
-        $disk_usage_status = "success";
-    }
-    ?>
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-primary">
-                <div class="panel-heading"><i class="fa fa-cube fa-fw"></i> System</div>
-                <div class="panel-body">
+	// disk usage
+	if ($dp >= 90) {
+		$disk_usage_status = "danger";
+	} elseif ($dp >= 70 && $dp < 90) {
+		$disk_usage_status = "warning";
+	} else {
+		$disk_usage_status = "success";
+	}
+	?>
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading"><i class="fa fa-cube fa-fw"></i> System</div>
+				<div class="panel-body">
 
-                    <?php
-                    if (isset($_POST['system_reboot'])) {
-                        echo '<div class="alert alert-warning">System Rebooting Now!</div>';
-                        $result = shell_exec("sudo /sbin/reboot");
-                    }
-                    if (isset($_POST['system_shutdown'])) {
-                        echo '<div class="alert alert-warning">System Shutting Down Now!</div>';
-                        $result = shell_exec("sudo /sbin/shutdown -h now");
-                    }
-		    if (isset($_POST['service_start'])) {
-                        echo '<div class="alert alert-warning">allsky service started</div>';
-                        $result = shell_exec("sudo /bin/systemctl start allsky");
-                    }
-		    if (isset($_POST['service_stop'])) {
-                        echo '<div class="alert alert-warning">allsky service stopped</div>';
-                        $result = shell_exec("sudo /bin/systemctl stop allsky");
-                    }
-                    ?>
+					<?php
+					if (isset($_POST['system_reboot'])) {
+						echo '<div class="alert alert-warning">System Rebooting Now!</div>';
+						$result = shell_exec("sudo /sbin/reboot");
+					}
+					if (isset($_POST['system_shutdown'])) {
+						echo '<div class="alert alert-warning">System Shutting Down Now!</div>';
+						$result = shell_exec("sudo /sbin/shutdown -h now");
+					}
+					if (isset($_POST['service_start'])) {
+						echo '<div class="alert alert-warning">allsky service started</div>';
+						$result = shell_exec("sudo /bin/systemctl start allsky");
+					}
+					if (isset($_POST['service_stop'])) {
+						echo '<div class="alert alert-warning">allsky service stopped</div>';
+						$result = shell_exec("sudo /bin/systemctl stop allsky");
+					}
+					?>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-body">
-                                    <h4>System Information</h4>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<h4>System Information</h4>
 									<table>
 									<tr class="x"><td class="info-item">Hostname</td><td><?php echo $hostname ?></td></tr>
 									<tr class="x"><td class="info-item">Pi Revision</td><td><?php echo RPiVersion() ?></td></tr>
@@ -311,9 +311,9 @@ function DisplaySystem()
 										<td style="width: 100%" class="progress"><div class="progress-bar progress-bar-<?php echo $temperature_status ?>"
 										role="progressbar"
 										aria-valuenow="<?php echo $temperature ?>" aria-valuemin="0" aria-valuemax="100"
-                                        style="width: <?php echo $temperature ?>%;"><?php echo $display_temperature ?>
+										style="width: <?php echo $temperature ?>%;"><?php echo $display_temperature ?>
 										</div></td></tr>
-                                <?php if ($fan != "") { ?>
+								<?php if ($fan != "") { ?>
 
 									<tr><td colspan="2" style="height: 10px"></td></tr>
 									<tr><td class="info-item">Fan Speed</td>
@@ -322,7 +322,7 @@ function DisplaySystem()
 										aria-valuenow="<?php echo $fan ?>" aria-valuemin="0" aria-valuemax="100"
 										style="width: <?php echo $fan ?>%;"><?php echo $fan ?>%
 										</div></td></tr>
-                                <?php } ?>
+								<?php } ?>
 
 									<tr><td colspan="2" style="height: 10px"></td></tr>
 									<tr><td class="info-item">Disk Usage</td>
@@ -332,27 +332,27 @@ function DisplaySystem()
 										style="width: <?php echo $dp ?>%;"><?php echo $dp ?>%
 										</div></td></tr>
 									</table>
-                                </div><!-- /.panel-body -->
-                            </div><!-- /.panel-default -->
-                        </div><!-- /.col-md-6 -->
-                    </div><!-- /.row -->
+								</div><!-- /.panel-body -->
+							</div><!-- /.panel-default -->
+						</div><!-- /.col-md-6 -->
+					</div><!-- /.row -->
 
-                    <form action="?page=system_info" method="POST">
-			<div style="margin-bottom: 20px">
-				<button type="button" class="btn btn-outline btn-primary" onclick="document.location.reload(true)"><i class="fa fa-sync-alt"></i> Refresh</button>
-                        </div>
-			<div style="margin-bottom: 15px">
-				<button type="submit" class="btn btn-success" style="margin-bottom:5px" name="service_start"/><i class="fa fa-play"></i> Start allsky</button>
-				<button type="submit" class="btn btn-danger" style="margin-bottom:5px" name="service_stop"/><i class="fa fa-stop"></i> Stop allsky</button>
-			</div>
-                        <button type="submit" class="btn btn-warning" style="margin-bottom:5px" name="system_reboot"/><i class="fa fa-power-off"></i> Reboot Raspberry Pi</button>
-                        <button type="submit" class="btn btn-warning" style="margin-bottom:5px" name="system_shutdown"/><i class="fa fa-plug"></i> Shutdown Raspberry Pi</button>
-                    </form>
+					<form action="?page=system_info" method="POST">
+					<div style="margin-bottom: 20px">
+						<button type="button" class="btn btn-outline btn-primary" onclick="document.location.reload(true)"><i class="fa fa-sync-alt"></i> Refresh</button>
+					</div>
+					<div style="margin-bottom: 15px">
+						<button type="submit" class="btn btn-success" style="margin-bottom:5px" name="service_start"/><i class="fa fa-play"></i> Start allsky</button>
+						<button type="submit" class="btn btn-danger" style="margin-bottom:5px" name="service_stop"/><i class="fa fa-stop"></i> Stop allsky</button>
+					</div>
+					<button type="submit" class="btn btn-warning" style="margin-bottom:5px" name="system_reboot"/><i class="fa fa-power-off"></i> Reboot Raspberry Pi</button>
+					<button type="submit" class="btn btn-warning" style="margin-bottom:5px" name="system_shutdown"/><i class="fa fa-plug"></i> Shutdown Raspberry Pi</button>
+					</form>
 
-                </div><!-- /.panel-body -->
-            </div><!-- /.panel-primary -->
-        </div><!-- /.col-lg-12 -->
-    </div><!-- /.row -->
-    <?php
+				</div><!-- /.panel-body -->
+			</div><!-- /.panel-primary -->
+		</div><!-- /.col-lg-12 -->
+	</div><!-- /.row -->
+	<?php
 }
 ?>
